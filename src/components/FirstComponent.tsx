50c19f56f41623f72c43178cd0c808a42dd3890d
LindaBot <voidnullbot@outlook.com>
2018-11-07 20:55:32 +1300

    MSA Base project complete

---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+export default class FirstComponents extends React.Component{
+    public render(){
+        return (
+            <div className="centreText">
+                { /* React components have this wrapper */ }
+                <h1>(▀̿Ĺ̯▀̿ ̿)</h1>
+            </div>
+        );
+    }
+}
---
+++ src/components/SecondComponent.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class SecondComponents extends React.Component{
+    public render(){
+        return (
+            <div className="centreText">
+                <h1>༼ つ  ͡° ͜ʖ ͡° ༽つ</h1>
+            </div>
+        )
+    }
+}
