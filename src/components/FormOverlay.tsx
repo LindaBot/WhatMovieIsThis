f555bdeb7bd30bac0aca01cd300ad8d7f9a66e76
LindaBot <voidnullbot@outlook.com>
2018-11-08 23:17:09 +1300

    API call successful

--- src/components/FormOverlay.tsx
+++ src/components/FormOverlay.tsx
@@ -2,16 +2,17 @@ import * as React from "react"
 import SearchBar from './SearchBar';
 
 interface IStates{
+    movieInfo: any
     searchQuery?: any
-    movieInfo?: any
 }
 
 // interface IProps{}
 
-class FormOverlay extends React.Component<{}, IStates>{
-    constructor(states: IStates){
-        super(states);
+export default class FormOverlay extends React.Component<{}, IStates>{
+    constructor(props: any){
+        super(props);
         this.state = {
+            movieInfo: "",
             searchQuery: ""
         }
     }
@@ -20,13 +21,18 @@ class FormOverlay extends React.Component<{}, IStates>{
         this.setState({
             searchQuery: value
         });
-        this.getMovie;
+        this.getMovie();
     }
 
-    public getMovie = () => {
+    public getMovie = async () => {
         // API key: 55d9f3f4
         // Example call: http://www.omdbapi.com/?apikey=55d9f3f4&t=MOVIENAME
-        var apiCallString = "http://www.omdbapi.com/?apikey=55d9f3f4&t=" + this.state.searchQuery;
+        const apiCallString = "http://www.omdbapi.com/?apikey=55d9f3f4&t=" + this.state.searchQuery;
+        const data = await fetch(apiCallString);
+        const dataJSON = await data.json();         
+        this.setState({
+            movieInfo: JSON.stringify(dataJSON)
+        })
     }
     
 
@@ -35,9 +41,9 @@ class FormOverlay extends React.Component<{}, IStates>{
             <div className="formOverlay">
                 <SearchBar onSubmit={this.handleClick}/>
                 <p>This is the value we got: {this.state.searchQuery}</p>
+                <p>Movie info: {this.state.movieInfo}</p>
             </div>
         );
     }
 }
 
-export default FormOverlay;
