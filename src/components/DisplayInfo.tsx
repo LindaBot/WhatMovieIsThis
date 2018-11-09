cda4c7889b0cb60b789b895dfae543751a44638e
LindaBot <voidnullbot@outlook.com>
2018-11-09 13:38:48 +1300

    Refactored DisplayInfo for better style

--- src/components/DisplayInfo.tsx
+++ src/components/DisplayInfo.tsx
@@ -18,55 +18,49 @@ export default class DisplayInfo extends React.Component<IProps, IState>{
     
 
     public render() {
-        let validMovie;
-        if (this.props.dataJSON === JSON || this.props.dataJSON.Response === "False"){
-            // Empty JSON
-            validMovie = 0;
+        if (this.props.dataJSON.Response === "False")
+        {
+            return(
+                <div>
+                    <p>No movie was found</p>
+                </div>
+            )
+        } else if (this.props.dataJSON === JSON) {
+            return (null);
         } else {
-            validMovie = 1;
-        }
-        
-        return(
-            <div>
-                <p>
-                {validMovie === 1 
-                    ? 
+            return(
+                <div>
                     <div className="container">
                         <div className="jumbotron">
                             <h1>{this.props.dataJSON.Title}</h1>
                             <img src={this.props.dataJSON.Poster}/>
                         </div>
                         <div className="row marketing">
-                        <div className="col-lg-6">
-                            <h4>Genre</h4>
-                            <p>{this.props.dataJSON.Genre}</p>
+                            <div className="col-lg-6">
+                                <h4>Genre</h4>
+                                <p>{this.props.dataJSON.Genre}</p>
 
-                            <h4>Subheading</h4>
-                            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+                                <h4>Subheading</h4>
+                                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
 
-                            <h4>Subheading</h4>
-                            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-                        </div>
+                                <h4>Subheading</h4>
+                                <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                            </div>
                         
-                        <div className="col-lg-6">
-                            <h4>Genre</h4>
-                            <p>{this.props.dataJSON.Genre}</p>
+                            <div className="col-lg-6">
+                                <h4>Genre</h4>
+                                <p>{this.props.dataJSON.Genre}</p>
 
-                            <h4>Subheading</h4>
-                            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+                                <h4>Subheading</h4>
+                                <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
 
-                            <h4>Subheading</h4>
-                            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                                <h4>Subheading</h4>
+                                <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                            </div>
                         </div>
-                        </div>
-                    </div>
-                    :
-                    // JSON is empty
-                    <div>
-                        <p>no movie</p>
-                    </div>
-                }</p>
-            </div>
-        );
+                    </div>    
+                </div>
+            );
+        }
     }
 }
